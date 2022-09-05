add6527fb82c7a4402c19a48dd13daad9bd028f8
ledonobel <92601234+ledonobel@users.noreply.github.com>
2022-09-05 12:06:44 +0100

    Add files via upload

---
+++ nsndportfolio/pages/_document.tsx
@@ -0,0 +1,16 @@
+import{Html, Head, Main, NextScript} from 'next/document' 
+
+export default function document() {
+
+  return(
+    <Html className='dark'>
+      <Head>
+      </Head>
+      <body>
+        <main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
+  
+}
